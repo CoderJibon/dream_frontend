df5e891de72c1e1df1a827ac55abe71deb8ec495
coderjibon <contact.engjibon@gmail.com>
2024-07-17 12:58:26 +0600

    fix the work

--- src/pages/user/Work/Work.jsx
+++ src/pages/user/Work/Work.jsx
@@ -86,7 +86,9 @@ const Work = () => {
           work.length > 0 &&
           work.slice(0, limited).map((w, idx) => {
             // Check if this work ID exists in the user's clickAd
-            const isDisabled = clickAd.some((ad) => ad.Email === user.email);
+            const isDisabled = clickAd.some(
+              (ad) => ad.Email === user.email && ad.adID === w._id
+            );
             return (
               <div key={idx} className="workcontainer">
                 <div className="workflex">
